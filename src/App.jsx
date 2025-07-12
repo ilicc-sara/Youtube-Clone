9da1d47c342d09efce50998b6cac5cfeeddbb63c
Sara <sarailic431@gmail.com>
2025-07-12 21:22:03 +0200

    api request...

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      try {
+        const response = await fetch(
+          `https://rapidapi.com/search/Tools?sortBy=ByRelevance`
+        );
+        const videos = await response.json();
+        console.log(videos);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchPost();
+  }, []);
 
   return (
     <>
