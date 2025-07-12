c7e924692f3f8dac67748b2e7416e6f86fa6c713
Sara <sarailic431@gmail.com>
2025-07-12 20:53:59 +0200

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,11 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <h1>YouTube</h1>
+    </>
+  );
 }
 
 export default App;
